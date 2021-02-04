2fa2b21539b3f797c8e810ab6238057c9160de0c
yeqiang <289632785@qq.com>
2021-02-04 15:43:11 +0800

    顺利完成第五章编译

--- book/chapter-5.sh
+++ book/chapter-5.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 
 echo ">>> Chapter 5 - Build toolchain..."
+export MAKEFLAGS="-j 1"
 
 /book/chapter-5/5.4-make-binutils.sh
 /book/chapter-5/5.5-make-gcc.sh
+
+# /tmp/gcc/build/include/type_traits:921:64: error: there are no arguments to '__is_constructible' that depend on a template parameter, so a declaration of '__is_constructible' must be available [-fpermissive]
+export AR=/tools/bin/x86_64-lfs-linux-gnu-ar
+export AS=/tools/bin/x86_64-lfs-linux-gnu-as
+export CXXFILT=/tools/bin/x86_64-lfs-linux-gnu-c++filt
+export CXX=/tools/bin/x86_64-lfs-linux-gnu-g++
+export CC=/tools/bin/x86_64-lfs-linux-gnu-gcc
+export LD=/tools/bin/x86_64-lfs-linux-gnu-ld
+export RANLIB=/tools/bin/x86_64-lfs-linux-gnu-ranlib
+export PATH=/tools/bin:$PATH
+
 /book/chapter-5/5.6-make-linux-api-headers.sh
 /book/chapter-5/5.7-make-glibc.sh
 /book/chapter-5/5.8-make-libstdc.sh
